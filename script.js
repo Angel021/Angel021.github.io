ce9e278e937909f8f1ba110ca90dcc43d6da36ff
Angel021 <85586504+Angel021@users.noreply.github.com>
2022-08-02 12:10:54 -0500

    Poner audio

--- script.js
+++ script.js
@@ -174,7 +174,7 @@ btn.addEventListener("click", () => {
         vib(1);
         btn2.classList.toggle("openB2On");
         ringBtn.classList.toggle("ringtoneOn");
-        aud.classList.click("audio");
+        aud.play();
       }
       dayCnt.innerHTML = diffD;
       hrCnt.innerHTML = diffH;
